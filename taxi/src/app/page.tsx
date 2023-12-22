4c26c097bf6b0b66e20b67fb205a8986c98b6534
Sergey <morgan.up.dn@gmail.com>
2023-12-22 12:22:16 +0300

    Update page.tsx

--- taxi/src/app/page.tsx
+++ taxi/src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home (){
-  return <h1>Welcome home!</h1>
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome home!</h1>
+
+      <Link href="/products"> Products </Link>
+      <Link href="/about"> About </Link>
+      <Link href="/contacts"> Contacts </Link>
+    </>
+  );
 }
