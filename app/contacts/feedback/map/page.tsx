c210802f5e0cbbf0eab6d5a9fe996ff2634038e8
Sergey <morgan.up.dn@gmail.com>
2023-12-24 16:46:07 +0300

    Delete app directory

--- app/contacts/feedback/map/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Map (){
-  return <h1> Contacts  feedback map  post</h1>
-}
--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-function BarChar() {
-  return <h1>Bar chart</h1>;
-}
-
-
-export default function Dashboard () {
-  return <h1> Dashboard </h1>
-}
