88d1334e62d6ca72319f1df69356143efd609a2b
Sergey <morgan.up.dn@gmail.com>
2024-01-04 20:17:07 +0300

    Delete taxi/src/app/f1 directory

--- taxi/src/app/f1/(.)f2/page.tsx
+++
@@ -1,4 +0,0 @@
-
-export default function  InterceptedF2() {
-  return <h1> (.) InterceptedF2 page  </h1>;
-}
--- taxi/src/app/f1/f3/page.tsx
+++
@@ -1,6 +0,0 @@
-
-
-
-export default function  InterceptedF3() {
-  return <h1> F3 page  </h1>;
-}
